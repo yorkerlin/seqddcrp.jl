15e924335e7d380a22c0cac3abd10e8aa6cff500
Wu Lin <yorker.lin@gmail.com>
2015-05-24 19:40:22 -0400

    update demo

--- demo/vlb.jl
+++ demo/vlb.jl
@@ -1,21 +1,23 @@
 push!(LOAD_PATH, "../src/")
+push!(LOAD_PATH, "../src2/")
 
 using seqddCRP
 using Distributions
+using ConjugatePriors
+import ConjugatePriors.NormalWishart
 
-prior = NormalWishart(zeros(2), 1e-7, eye(2) / 4, 4.0001)
 #out = open("5.csv", "a") 
 
-x = readdlm("balley.csv", ',')
+x = readdlm("./balley.csv", ',')
 x = x'
 
-#x = x[:, 1:300]
-
 N = size(x, 2)
+p = size(x, 1)
+prior = NormalWishart(zeros(2), 1e-7, eye(2) / 4, 4.0001)
 
 F = crp_F(N, 1e-1)
 
-#crp, predictive_likelihood = var_gaussian_mixture(x, F, prior, singular_init_dist)
-#ll = infer(crp, 100)
+crp, predictive_likelihood = var_gaussian_mixture(x, F, prior, singular_init_dist)
+ll = infer(crp, 100)
 
 #println(ll)
